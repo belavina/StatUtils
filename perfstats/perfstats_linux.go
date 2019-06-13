95fbfcf2cfe9dab3eab967f269b67b8be62e00a3
Olga Belavina <ol.belavina@gmail.com>
2019-06-13 15:34:56 -0400

    Added free output parser

--- perfstats/perfstats_linux.go
+++ perfstats/perfstats_linux.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
 	"strconv"
 	"strings"
 	"time"
@@ -21,6 +22,15 @@ const (
 	procCPUSoftirq = iota
 	procCPUSteal   = iota
 	procCPUGuest   = iota
+)
+
+const (
+	memoryTotal     = iota
+	memoryUsed      = iota
+	memoryFree      = iota
+	memoryShared    = iota
+	memoryBuffCache = iota
+	memoryAvailable = iota
 )
 
 // convert proc time samples to float
@@ -68,6 +78,12 @@ func parseProcStat() map[string][]float64 {
 	return cpuStats
 }
 
+func getDateFormatted() string {
+	// match date format returned by win
+	dt := time.Now()
+	return dt.Format("1/2/2006 3:04:05 PM")
+}
+
 // compute active and total CPU utilizations from proc/stat readings
 func computeActiveTotalCPU(procStats map[string][]float64) (map[string]float64, map[string]float64) {
 
@@ -99,10 +115,7 @@ func getCPUStats() []SysStat {
 	var sysStats []SysStat
 	var statEntry SysStat
 
-	// match date format returned by win
-	dt := time.Now()
-	dateFormatted := dt.Format("1/2/2006 3:04:05 PM")
-
+	dateFormatted := getDateFormatted()
 	timeBetweenSamples := 2 * time.Second
 
 	// sample 2 stats with a time-delay in between
@@ -126,11 +139,40 @@ func getCPUStats() []SysStat {
 	return sysStats
 }
 
+func getMemoryStats() SysStat {
+
+	var memStat SysStat
+
+	const (
+		header = iota
+		memory = iota
+		swap   = iota
+	)
+
+	cmdResult := exec.Command("free", "--bytes")
+
+	out, err := cmdResult.Output()
+	if err != nil {
+		fmt.Println(err)
+		return memStat
+	}
+
+	// get total, used, free etc. (discard first token sicne it's a label "Mem:")
+	memInfo := strings.Fields(strings.Split(string(out[:]), "\n")[memory])[1:]
+
+	memStat.Date = getDateFormatted()
+	memStat.Key = "Memory Available"
+	memStat.Value = memInfo[memoryAvailable]
+
+	return memStat
+}
+
 // PlatformSysStats Query performance stats on linux platform
 func PlatformSysStats() []byte {
 
 	var stats []SysStat
 	stats = append(stats, getCPUStats()...)
+	stats = append(stats, getMemoryStats())
 
 	jsonData, err := json.Marshal(stats)
 	if err != nil {
